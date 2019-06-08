15e2dbf4d433a80a952ec572d44a30968f417566
Ola Næss Kaldestad <mail@okal.no>
2019-06-09 00:30:30 +0200

    rewrite as classes for readability

--- src/index.ts
+++ src/index.ts
@@ -1,123 +1,74 @@
+type Primitive = bigint | boolean | null | number | string | symbol | undefined;
+type NotFunction = Primitive | { [key: string]: unknown; } | unknown[];
 
-/**
- * match-on
- *
- * Aims to replace switch statements with a more functional style
- */
+type Predictor<T> = (value: T) => boolean;
+type Predicate<T> = Predictor<T> | T;
 
-/**
- * Generics
- *
- * M: (Match)
- * The type of the param passed to the 'match' function
- * 
- * R_I: (Result - Input)
- * The type of the second param passed to the 'on' function, or the param
- * passed to the 'default' function
- * 
- * R_O: (Result - Output)
- * The type of the final result after processing the second param passed to the
- * 'on' function. Equal to ResOut<R_I>
- * 
- * F_O: (Function - Output)
- * The inferred return type of R_I if it is a function
- */
+type Result<T> = ((value: T) => unknown) | NotFunction;
+type ResultValue<T, U> = U extends ((value: T) => infer V) ? NoVoid<V> : U;
+type NoVoid<T> = T extends void ? undefined : T;
 
-/**
- * NotFunction
- * 
- * Helper type that describes all types except Function
- */
+class Match<T> {
+    constructor(private readonly value: T) { }
 
-type NotFunctionValue = symbol | boolean | string | number | null | undefined;
+    private isPredictor(pred: Predicate<T>): pred is Predictor<T> {
+        return typeof(pred) === 'function';
+    }
 
-interface NotFunctionObject { [key: string]: NotFunction }
+    private evaluate(pred: Predicate<T>) {
+        return this.isPredictor(pred)
+            ? pred(this.value)
+            : pred === this.value;
+    }
 
-interface NotFunctionArray extends Array<NotFunction> { }
+    private createResult<U>(res: U): ResultValue<T, U> {
+        return typeof res === 'function'
+            ? res(this.value)
+            : res;
+    }
 
-type NotFunction = NotFunctionValue | NotFunctionObject | NotFunctionArray;
+    on<U extends Result<T>>(pred: Predicate<T>, res: U) {
+        return this.evaluate(pred)
+            ? new Matched(this.createResult(res))
+            : this;
+    }
 
-/**
- * Prediction
- * 
- * A Prediction<M> can either be a function (Predictor<M>) that evaluates to a
- * boolean, or a value of type M that is to be compared to the value passed to
- * the match function
- */
+    default<U extends Result<T>>(res: U) {
+        return new Defaulted(this.createResult(res));
+    }
 
-type Predictor<M> = (m: M) => boolean;
+    result() {
+        return undefined;
+    }
+}
 
-const isPredictor = <M>(p: Prediction<M>): p is Predictor<M> =>
-    typeof(p) === 'function';
+class Matched<T> {
+    constructor(private readonly res: T) { }
 
-type Prediction<M> = Predictor<M> | M;
+    on() {
+        return this;
+    }
 
-const prediction = <M>(p: Prediction<M>, x: M) =>
-    isPredictor(p) ? p(x) : p === x;
- 
-/**
- * Result
- * 
- * A Result<M> can either just be a value, or a function, in which case it will
- * be evaluated
- */
+    default() {
+        return new Defaulted(this.res);
+    }
 
-type ResIn<M> = ((m: M) => void) | NotFunction;
+    result() {
+        return this.res;
+    }
+}
 
-type ResOut<R_I, M> = R_I extends (m: M) => infer F_O ? NoVoid<F_O> : R_I;
+class Defaulted<T> {
+    constructor(private readonly res: T) { }
 
-type NoVoid<F_O> = F_O extends void ? undefined : F_O;
+    result() {
+        return this.res;
+    }
+}
 
-const result = <R_I, M>(r: R_I, m: M): ResOut<R_I, M> =>
-    typeof r === 'function' ? r(m) : r;
-
-/**
- * Matched / Defaulted
- * 
- * This mocks the main match function, and will simply propagate the value that
- * has been found to match
- */
-
-type Matched<R_O> = {
-    on: () => Matched<R_O>;
-    default: () => WithResult<R_O>;
-} & WithResult<R_O>;
-
-const matched = <R_O>(r: R_O): Matched<R_O> => ({
-    on: () => matched(r),
-    default: () => defaulted(r),
-    result: () => r
-});
-
-type WithResult<R_O> = {
-    result: () => R_O;
-};
-
-const defaulted = <R_O>(r: R_O): WithResult<R_O> => ({
-    result: () => r
-});
-
-/**
- * Match
- * 
- * This is the main function, that takes a value and uses function chaining to
- * check for different cases, as well as to provide a default case and an option
- * to get the result
- */
-
-type Match<M> = {
-    on: <R_I extends ResIn<M>>(p: Prediction<M>, r: R_I) => Matched<ResOut<R_I, M>> | Match<M>,
-    default: <R_I extends ResIn<M>>(d: R_I) => WithResult<ResOut<R_I, M>>
-} & WithResult<undefined>;
-
-const match = <M>(m: M): Match<M> => ({
-    on: (p, r) => prediction(p, m)
-        ? matched(result(r, m))
-        : match(m),
-    default: d => defaulted(result(d, m)),
-    result: () => undefined
-});
+function match<T>(value: T) {
+    return new Match(value);
+}
 
 export default match;
-
 export { match };
